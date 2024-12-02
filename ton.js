91258a470385df7066a7706d8a9e4503b2c8b31c
hono00 <141104053+hono00@users.noreply.github.com>
2024-12-02 14:41:37 +0100

    Add files via upload

---
+++ ton.js
@@ -0,0 +1,27 @@
+import TonConnectUI from '@tonconnect/ui';
+
+const tonConnectUI = new TonConnectUI({
+    manifestUrl: 'https://hono00.github.io/anyway/tonconnect-manifest.json',
+    buttonRootId: 'ton-connect'
+});
+
+async function sendTransaction() {
+    const transaction = {
+        messages: [
+            {
+                address: "UQBDe4HJeGDFRS4kAYhl5HZUfUYi1uYAbg6PRVnKZGrwVYHi", // adresse de destination
+                amount: "20000000" // Toncoin en nanotons
+            }
+        ]
+    };
+
+    try {
+        const result = await tonConnectUI.sendTransaction(transaction);
+        console.log(result);
+    } catch (error) {
+        console.error("Transaction échouée :", error);
+    }
+}
+
+// Appeler la fonction pour envoyer la transaction
+sendTransaction();
