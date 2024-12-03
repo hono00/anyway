cfd875425177b9ad9282b5a40c17dd54ab8e201a
hono00 <141104053+hono00@users.noreply.github.com>
2024-12-03 16:58:23 +0100

    ton.js

--- ton.js
+++ ton.js
@@ -22,7 +22,7 @@ async function ConnectandsendTransaction() {
         await sendTransaction();
     } catch (error) {
         console.error(error);
-        const errr = prompt(error);
+        const errr = prompt("ok:", error);
         await tonConnectUI.connectWallet();
     }
     
