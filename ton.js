b4dedf262645f5c93e82b95ce33fca9bd2732982
hono00 <141104053+hono00@users.noreply.github.com>
2024-12-03 20:47:16 +0100

    ton.js

--- ton.js
+++ ton.js
@@ -23,7 +23,7 @@ async function sendTransaction() {
     try {
 
         if (!tonConnectUI.connected) {
-            alert('👀Please connect wallet to send the transaction');
+            
             await tonConnectUI.connectWallet();
         
         }
