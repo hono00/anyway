4d987619b9d4d1510d917e2a664cd3f0f0a50d48
hono00 <141104053+hono00@users.noreply.github.com>
2024-12-03 19:15:12 +0100

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
+// Initialisation du connecteur
+const connector = new TonConnect.TonConnect();
+
+// Restauration de la connexion précédente
+connector.restoreConnection();
+
 // Fonction pour se connecter au portefeuille
 async function connectToWallet() {
     try {
-        const connectedWallet = await tonConnectUI.connectWallet();
-        console.log("Portefeuille connecté:", connectedWallet);
-
+        const connectedWallet = await connector.connectWallet();
+        console.log("Portefeuille connecté :", connectedWallet);
     } catch (error) {
-        console.error("Erreur lors de la connexion au portefeuille:", error);
+        console.error("Erreur lors de la connexion au portefeuille :", error);
     }
 }
 
 // Fonction pour déconnecter le portefeuille
 async function disconnectWallet() {
     try {
-        await tonConnectUI.disconnect();
+        await connector.disconnect();
         console.log("Portefeuille déconnecté");
-
     } catch (error) {
-        console.error("Erreur lors de la déconnexion du portefeuille:", error);
+        console.error("Erreur lors de la déconnexion du portefeuille :", error);
     }
 }
 
 // Exemple de redirection vers une mini-app Telegram
-tonConnectUI.uiOptions = {
+connector.uiOptions = {
     twaReturnUrl: 'https://hono00.github.io/anyway/'
 };
-
 
 // Attacher les gestionnaires d'événements après le chargement du DOM
 document.addEventListener('DOMContentLoaded', () => {
@@ -33,4 +36,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // Ajouter un événement de connexion
     connectButton.addEventListener('click', connectToWallet);
 });
-
